32aa748a94be1cc78605f6abb8e0e6485dbf58bb
ivarkaram <59892024+ivarkaram@users.noreply.github.com>
2020-09-30 22:01:14 +0200

    new scrip

---
+++ counting_nucleotides/main.py
@@ -0,0 +1,14 @@
+out=open('out_rna.txt', 'w+')
+
+with open ('rosalind_rna.txt', 'r') as f:
+    
+    r={'A':'A', 'C':'C', 'G':'G', 'T':'U' }
+    
+    seq=f.read().strip()
+    
+    for nucl in range(0,len(seq),1):
+        nucl=seq[nucl]
+        rna="" 
+        rna+=r[nucl]        
+        
+        out.write(rna)
